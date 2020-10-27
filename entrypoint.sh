4e8eceda5e28668728ad068b1e988e67d7d335fa
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-10-27 09:26:31 +0100

    Output the number of pages and file size

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,9 @@ echo
 ./tools/check_uncited_references.bash references.bib *.tex
 ./tools/check_unreferenced_labels.bash *.tex
 ./tools/check_missing_refs.bash
+
+echo
+nb_pages=$(pdfinfo thesis.pdf | grep Pages | sed 's/[^0-9]*//') 2> /dev/null
+echo "::set-output name=nb_pages::$nb_pages"
+file_size=$(du -sh thesis.pdf | cut -f1)
+echo "::set-output name=file_size::$file_size"
